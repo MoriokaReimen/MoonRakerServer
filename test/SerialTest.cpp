b14acbe6c0babb1efc89170d3540ced81031ec92
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-01-10 11:19:08 +0900

    modify bug

--- test/SerialTest.cpp
+++ test/SerialTest.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "motor.hpp"
 #include "data.hpp"
 using std::cout;
@@ -6,6 +8,7 @@ using std::endl;
 
 int main()
 {
+  const auto wait_time = std::chrono::milliseconds(1000);
   Motor motor;
   MotorData data;
   while(true)
@@ -20,6 +23,7 @@ int main()
     cout << data.front_rpm << ",";
     cout << data.battery_v << ",";
     cout << data.time << endl;
+    std::this_thread::sleep_for(wait_time);
   }
 
   return 0;
