4c65bc9576b54258502144fe164c7578078fbdf4
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-01-30 17:02:29 +0900

    Complete Simple Client

--- src/SimpleMain.cpp
+++ src/SimpleMain.cpp
@@ -4,12 +4,18 @@
 #include "motor.hpp"
 #include "data.hpp"
 using std::cout;
+using std::cin;
 using std::endl;
 
 int main()
 {
 	int ch = 0;
   int line = 0;
+  int left_rpm, right_rpm;
+  cout << "Enter Left RPM" << endl;
+  cin >> left_rpm;
+  cout << "Enter Right RPM" << endl;
+  cin >> right_rpm;
 
 	initscr();
 	cbreak();
@@ -21,20 +27,26 @@ int main()
 	while ((ch = getch()) != 'q') {
     try {
     data = motor.getData();
-    } catch(...){}
+    } catch(...)
+    {
+      move(1, 0);
+      printw("Error !!");
+    }
+    motor.sendCommand(MotorCommand(left_rpm, right_rpm));
     move(8, 0);
-		printw("Rear Current\tFront Current\tRear RPM\tFront RPM");
+		printw("Device\tRear Current\tFront Current\tRear RPM\tFront RPM");
     move(9 + line, 0);
-		printw("%5f\t%5f\t%5f\t%5f",
-        data.rear_current, data.front_current,
+		printw("%s\t%5f\t%5f\t%5f\t%5f",
+        data.device.c_str(), data.rear_current, data.front_current,
         data.rear_rpm, data.front_rpm);
 
 		move(6, 0);
 		printw("press (q) to quit");
     line++;
-    if(line > 20) line = 0;
+    if(line > 30) break;
 		refresh();
   }
+  motor.sendCommand(MotorCommand(0, 0));
 
 	endwin();
   return 0;
