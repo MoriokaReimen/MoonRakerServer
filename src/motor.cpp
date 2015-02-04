604686f63144d5fca51ae4084b5acb568a5861bb
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-02-04 14:35:25 +0900

    mod bugs

--- src/motor.cpp
+++ src/motor.cpp
@@ -68,16 +68,16 @@ int Motor::sendCommand(const MotorCommand& command)
 MotorData Motor::getData()
 {
     DataBytes bytes;
-    unsigned char buffer[40];
+    char buffer[40];
     char pattern[2] = {0x75, 0xFF};
     //serial.readUntil(reinterpret_cast<char*>(buffer), 40, pattern, 2);
-    serial.read((char*)buffer, 40, true, 100);
+    serial.read(buffer, 40, true, 100);
     serial.poll();
 
     //! Detect Headers and footers
     for (int i = 0; i < 40; ++i) {
-        if ((buffer[i] == 0x75) && (buffer[i + 1] == 0xAA) &&
-            (buffer[i + 17] == 0x75) && (buffer[i + 18] == 0xFF)) {
+        if (((unsigned char)buffer[i] == 0x75) && ((unsigned char)buffer[i + 1] == 0xAA) &&
+            ((unsigned char)buffer[i + 17] == 0x75) && ((unsigned char)buffer[i + 18] == 0xFF)) {
             //! motor data array
             memcpy(&bytes, &buffer[i], sizeof(bytes));
             return MotorData(bytes);
