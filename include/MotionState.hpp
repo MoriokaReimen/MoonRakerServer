5c706c25bab6c70c49aeb750cc45fca2a6594038
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-06-02 10:43:16 +0900

    complete motion state class

--- include/MotionState.hpp
+++ include/MotionState.hpp
@@ -34,27 +34,52 @@
 -----------------------------------------------------------------------------
 */
 #pragma once
-#include "Motor.hpp"
-#include "Command.hpp"
 #include "Data.hpp"
+#include <chrono>
 
 class WheelState
 {
 public:
-  rpm;
-  torque;
+  double rpm{0};
+  double current{0};
 };
+
 /*! @class Motor
  *  @brief handles serial communication between motor
 */
 class RoverState
 {
+  const double GEAR_RATIO = 690.0f;
+  const double GEAR_EFFICIENCY = 0.49f;
+  const double TORQUE_CONSTANT = 0.00902f * GEAR_RATIO * GEAR_EFFICIENCY; //! in [mNm/mA]
+  const double PI = 3.14159265359;
 public:
-  start;
-  now;
+  void set(const MotorData& left, const MotorData& right);
+  long long now{0};
   WheelState left_front;
   WheelState left_rear;
   WheelState right_front;
   WheelState right_rear;
-  //Quaternion quat; //! implement someday
+  //Quaternion quat; implement someday
 };
+
+void set(const MotorData& left, const MotorData& right)
+{
+  /* set rpm */
+  this->left_front.rpm = left.front_rpm / GEAR_RATIO;
+  this->left_rear.rpm = left.rear_rpm / GEAR_RATIO;
+  this->right_front.rpm = right.front_rpm / GEAR_RATIO;
+  this->right_rear.rpm = right.rear_rpm / GEAR_RATIO;
+
+  /* set torque */
+  this->left_front.torque = left.front_current * TORQUE_CONSTANT;
+  this->left_rear.torque = left.rear_current * TORQUE_CONSTANT;
+  this->right_front.torque = right.front_current * TORQUE_CONSTANT;
+  this->right_rear.torque = right.rear_current * TORQUE_CONSTANT;
+
+  /* set time */
+  this->now = std::chrono::duration_cast<std::chrono::milliseconds>
+    (std::chrono::system_clock::now().time_since_epoch()).count();
+
+  return;
+}
